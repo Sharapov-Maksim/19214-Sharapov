ce627c21e14f57c54377712e39d477e348c4187f
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2019-10-16 16:02:24 +0700

    Update lists.hs

--- lists.hs
+++ lists.hs
@@ -1,4 +1,4 @@
-get::[a] -> Int -> a
+get::[a] -> Integer -> a
 get [] _ = error "no such element"
 get (x:xs) 0 = x
 get (x:xs) n = get xs (n-1)
@@ -25,13 +25,13 @@ init' [] = error "empty list"
 init' [x] = []
 init' (x:xs) = x:init' xs
 
-
+-- не оптимально (можно за N)
 reverse':: [a] -> [a]
 reverse' [] = []
 reverse' xs = last' xs : reverse'(init' xs)
 
-
-length':: [a] -> Int
+--хвостовую
+length':: [a] -> Integer
 length' [] = 0
 length' (x:xs) =1 + length' xs
 
@@ -39,30 +39,33 @@ length' (x:xs) =1 + length' xs
 append' :: [a] -> a -> [a]
 append' [] x = [x]
 append' (y:xs) x = y : append' xs x
+
 --append' (y:xs) x = if length' xs == 0 then y:x:xs else y : append' xs x
 --append' xs x = reverse'(x:(reverse' xs)) 
 
-
+--можно сделать быстрее (за длину первого списка)
 concat' :: [a] -> [a] -> [a]
 concat' xs [] = xs
-concat' xs (y:ys) = concat' (append xs y) ys 
+concat' xs (y:ys) = concat' (append' xs y) ys 
 
 
-drop' :: Int -> [a] -> [a]
+drop' :: Integer -> [a] -> [a]
 drop' _ [] = [] -- or error
 drop' 0 xs = xs
 drop' n (x:xs) = drop' (n-1) xs
 
 
-take' :: Int -> [a] -> [a]
+take' :: Integer -> [a] -> [a]
 take' _ [] = [] -- or error
 take' 0 xs = []
 take' n (x:xs) = x : take' (n-1) xs
 
 
-splitAt' :: Int -> [a] -> ([a],[a])
---splitAt' _ [] = error
-splitAt' n xs = (take n xs, drop n xs)
+splitAt' :: Integer -> [a] -> ([a],[a])
+splitAt' n xs = (take' n xs, drop' n xs)
+--splitAt' _ [] = error "empty list"
+--splitAt' n xs = if n > length' xs then error "not enogh elements" 
+--else (take' n xs, drop' n xs)
 
 
 null' :: [a] -> Bool
@@ -77,7 +80,7 @@ elem' (x:xs) y = if x == y then True else elem' xs y
 
 filter' :: (a -> Bool) -> [a] -> [a]
 filter' _ [] = []
-filter' test (x:xs) = if test x == True then x:filter' test xs
+filter' test (x:xs) = if test x  then x:filter' test xs
                                         else filter' test xs
 
 
@@ -91,13 +94,3 @@ zip' [] [] = []
 zip' [x] [] = []
 zip' [] [y] = []
 zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
-
-
-
-
-
-
-
-
-
-
