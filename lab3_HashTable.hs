477563a93a501bd09fef5fc05b4f792f36a1bfd2
Sharapov-Maksim <55018975+Sharapov-Maksim@users.noreply.github.com>
2019-12-24 21:32:03 +0700

    Update lab3_HashTable.hs

--- lab3_HashTable.hs
+++ lab3_HashTable.hs
@@ -1,5 +1,6 @@
 import Data.Char
 import System.IO
+import Data.Tuple
 
 hash :: String -> Capacity -> Integer
 hash key maxSize = (foldl (\acc v -> (acc + v)) 0 (map (\ v -> toInteger(ord v)) key)) `mod` maxSize
@@ -18,13 +19,13 @@ defaultHashTable cap = (ToHashtable (replicate (fromInteger(cap)) []) cap 0)
 loadFactor :: Hashtable k v -> LoadFactor
 loadFactor (ToHashtable _ cap size) = (fromInteger size / fromInteger cap)
 
-
 insert :: (Show k, Eq k) =>  Hashtable k v -> k -> v -> Hashtable k v
-insert (ToHashtable table cap size) key value = if loadFactor (ToHashtable table cap size) > 0.9 then 
+insert (ToHashtable table cap size) key value = if loadFactor (ToHashtable table cap size) > 0.5 then 
                                     insert (rehash (ToHashtable table size cap)) key value else
                                     (ToHashtable (left ++ mid ++ (drop 1 right)) cap (size+1)) where
-                                            mid = [(table !! fromInteger(hash (show key) cap)) ++ [(key, value)]]
                                             (left, right) = splitAt (fromInteger(hash (show key) cap)) table
+                                            mid = [(key, value):head right]
+                                            
 
 rehash ::(Show k, Eq k) => Hashtable k v -> Hashtable k v
 rehash (ToHashtable table cap _) = foldl (\acc (k,v) -> insert acc k v) (defaultHashTable (cap * 3)) (concat table)
@@ -36,15 +37,31 @@ fromList table = foldl (\acc (k,v) -> (insert acc k v)) (defaultHashTable 15) ta
 clear::Hashtable k v -> Hashtable k v
 clear table = defaultHashTable 15
 
+
 erase::(Show k, Eq k)=>Hashtable k v->k->Hashtable k v
-erase (ToHashtable table cap size) key = (ToHashtable (left ++ mid ++ (drop 1 right)) cap (size+1)) where
-                                            mid = [[]]
+erase (ToHashtable table cap size) key = (ToHashtable (left ++ mid ++ (drop 1 right)) cap (size-1)) where
+                                            
                                             (left, right) = splitAt (fromInteger(hash (show key) cap)) table
+                                            mid = [filter (\(k, v) -> k/= key) (head right)]
+
+
 
 contains::(Show k, Eq k)=>Hashtable k v -> k -> Bool
-contains (ToHashtable table cap _) key = elem key (foldl (\acc (k,v) -> acc ++ k) [] (table !! fromInteger(hash (show key) cap)))
+contains (ToHashtable table cap _) key = elem key (map (fst) (table !! fromInteger(hash (show key) cap)))
+
+at::(Show k, Eq k)=>Hashtable k v -> k -> Maybe v
+at (ToHashtable table cap _) key = if null x
+                                   then Nothing
+                                   else Just (snd (head x))
+                                   where
+                                        x = filter (\(k,v) -> k == key) (table !! fromInteger(hash (show key) cap))
 
 
+size::(Show k, Eq k)=>Hashtable k v -> Integer
+size (ToHashtable _ _ size) = size
+
+empty::(Show k, Eq k)=>Hashtable k v -> Bool
+empty (ToHashtable _ _ size) = size == 0
 
 main = do
     handle <- openFile "input.txt" ReadMode
